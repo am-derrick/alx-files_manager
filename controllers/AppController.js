8d1e2ac13523eff6b29789bc6effccf891b209ea
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-06-16 10:34:58 +0300

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,21 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
-    if (redisClient.isAlive() && dbClient.isAlive()) {
-      res.status(200).json({ redis: true, db: true });
-    }
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
   }
 
-  static async getStats(req, res) {
-    const files = await dbClient.nbFiles();
-    const users = await dbClient.nbUsers();
-    res.status(200).json({ users, files });
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
   }
 }
 
